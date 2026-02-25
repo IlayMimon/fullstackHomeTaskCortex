07312b76498cee55cdbd76fa51d7deb82750ef84
Ilay Maimon <ilaymimon@gmail.com>
2026-02-25 20:39:11 +0200

    coras

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import { setGlobalOptions } from "firebase-functions";
 import { onRequest } from "firebase-functions/https";
 import express from "express";
-import cors from "cors";
 import "./config/firebase";
 import trafficRoutes from "./routes/trafficEntries.routes";
 
@@ -11,15 +10,19 @@ const app = express();
 
 const allowedOrigins = ["https://fullstackhometaskcortex.web.app"];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (origin && allowedOrigins.includes(origin)) return callback(null, true);
-      callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  }),
-);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  }
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  return next();
+});
 
 app.use(express.json());
 app.use("/trafficEntries", trafficRoutes);
