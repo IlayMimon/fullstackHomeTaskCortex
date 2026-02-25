5935bee7f57d4a15d7add77e28464d04126b45ff
Ilay Maimon <ilaymimon@gmail.com>
2026-02-25 22:25:58 +0200

    fix cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,15 @@
 import { setGlobalOptions } from "firebase-functions";
 import { onRequest } from "firebase-functions/https";
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import "./config/firebase";
 import trafficRoutes from "./routes/trafficEntries.routes";
 
 setGlobalOptions({ maxInstances: 10 });
 
 const app = express();
-const allowedOrigins = [
-  "https://fullstackhometaskcortex.web.app",
-  "https://fullstackhometaskcortex.web.app/",
-];
+const allowedOrigins = ["https://fullstackhometaskcortex.web.app"];
 
-app.options("*", (req: Request, res: Response) => {
-  const origin = req.headers.origin || "";
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    return res.sendStatus(204);
-  }
-  return res.sendStatus(403);
-});
-
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((req, res, next) => {
   const origin = req.headers.origin || "";
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
@@ -32,7 +17,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
   }
-  next();
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  return next();
 });
 
 app.use(express.json());
